319ab3cdd39c8cbb42b635ac997c5c7bef6253fe
Banisaputra <bani.s26shafa@gmail.com>
2024-05-29 16:32:23 +0700

    fix: modal bug

--- app/Views/admin/armada.php
+++ app/Views/admin/armada.php
@@ -16,7 +16,7 @@
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel3">Detail Armada</h5>
+        <h5 class="modal-title">Detail Armada</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body" id="detailArmada">
--- app/Views/user/layout/main.php
+++ app/Views/user/layout/main.php
@@ -97,24 +97,6 @@
           </div>
         </nav>
 
-        <!-- modal view armada -->
-        <div class="modal fade" id="viewArmada" tabindex="-1" aria-hidden="true">
-          <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel3">Detail Armada</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body" id="detailArmada">
-                <!-- modal body -->
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal"> Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
-
         <div class="content-wrapper">
           <div class="container-xxl flex-grow-1 container-p-y">
 
