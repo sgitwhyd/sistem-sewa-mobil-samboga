8898e1ab43d98a06e92d06764bfc0fce831f2fef
Banisaputra <bani.s26shafa@gmail.com>
2024-10-14 08:54:32 +0700

    mod: dashboard admin

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,6 +3,9 @@
 namespace App\Controllers;
 
 use App\Models\Vehicles;
+use App\Models\Users;
+use App\Models\Transactions;
+use App\Models\Banks;
 
 class Home extends BaseController
 {
@@ -24,7 +27,19 @@ class Home extends BaseController
 
     public function admin() 
     {
-        return view('admin/dashboard');
+        $vehicle = new Vehicles();
+        $user = new Users();
+        $transaction = new Transactions();
+        $bank = new Banks();
+        
+        $data = [
+            'total_vehicles' => $vehicle->countAllResults(),
+            'total_users' => $user->countAllResults(),
+            'total_transactions' => $transaction->countAllResults(),
+            'total_banks' => $bank->countAllResults()
+        ];
+        
+        return view('admin/dashboard', $data);
     }
 
     public function user()
